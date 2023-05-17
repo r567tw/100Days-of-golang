f702cf07579db027f1fcbce28198cae8dd2ed0f1
r567tw <r567tw@gmail.com>
2023-05-17 21:36:02 +0800

    Day31

--- Day-31/main.go
+++ Day-31/main.go
@@ -26,6 +26,26 @@ func (list *LinkedList) Add(data string) {
 	}
 }
 
+func (list *LinkedList) Remove(data string) {
+	if list.Head == nil {
+		return
+	}
+
+	if list.Head.Data == data {
+		list.Head = list.Head.Next
+		return
+	}
+
+	current := list.Head
+	for current.Next != nil {
+		if current.Next.Data == data {
+			current.Next = current.Next.Next
+			return
+		}
+		current = current.Next
+	}
+}
+
 func (list *LinkedList) length() int {
 	result := 0
 	for current := list.Head; current != nil; current = current.Next {
@@ -45,5 +65,13 @@ func main() {
 	for current := names.Head; current != nil; current = current.Next {
 		fmt.Println(current.Data)
 	}
+	fmt.Printf("Length: %d\n", names.length())
+
+	names.Remove("Jacky")
+
+	for current := names.Head; current != nil; current = current.Next {
+		fmt.Println(current.Data)
+	}
+
 	fmt.Printf("Length: %d\n", names.length())
 }
