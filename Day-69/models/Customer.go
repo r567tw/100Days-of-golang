44a7c3daeeb907d13154696274a0762be1decdef
r567tw <r567tw@gmail.com>
2023-09-05 23:48:14 +0800

    69-update

--- Day-69/models/Customer.go
+++ Day-69/models/Customer.go
@@ -1,12 +1,15 @@
 package models
 
 import (
-	_ "gorm.io/gorm"
+	"gorm.io/gorm"
 )
 
 type Customer struct {
 	ID     uint   `json:"id" gorm:"primary_key"`
 	Name      string `json:"username"`
 	Bio		  string ` json:"bio"`
-	Account   string `json:"account"`
+	CreatedAt int64      `gorm:"autoCreateTime" json:"created_at"`
+    UpdatedAt int64      `gorm:"autoUpdateTime" json:"updated_at"`
+    DeletedAt gorm.DeletedAt `json:"deleted_at" gorm:"index"`
+
 }
