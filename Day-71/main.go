7b0f368ef24f2d8d3d5a8a55202ff654b4732b3d
r567tw <r567tw@gmail.com>
2023-10-25 04:11:56 +0800

    day71

---
+++ Day-71/main.go
@@ -0,0 +1,164 @@
+package main
+
+import (
+	"fmt"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+
+	"github.com/gin-gonic/gin"
+	"net/http"
+
+	"day-71/models"
+)
+
+var DB *gorm.DB
+
+func main() {
+	//Connection
+	conn, err := gorm.Open(mysql.Open("root:root@tcp(localhost:3306)/test?charset=utf8"), &gorm.Config{})
+	if err != nil {
+		fmt.Println("connection to mysql failed:", err)
+		return
+	}
+
+	//判斷有沒有table存在
+	migrator := conn.Migrator()
+	has := migrator.HasTable(&models.Customer{})
+	if !has {
+		//產生table
+		conn.Debug().AutoMigrate(&models.Customer{})
+	}
+
+	DB = conn
+
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "ping",
+		})
+	})
+	
+	// list 
+	r.GET("/customers", getCustomers) 
+	// create
+	r.POST("/customers", createCustomer)
+	// read
+	r.GET("/customers/:id", getCustomer)
+	// update
+	r.PUT("/customers/:id", updateCustomer)
+	// delete
+	r.DELETE("/customers/:id", deleteCustomer)
+
+	r.Run(":8888")
+}
+
+func getCustomers(context *gin.Context) {
+	var customers []models.Customer
+	var customerData []struct {
+		ID uint `json:"id"`
+		Name string `json:"name"`
+		Bio  string `json:"bio"`
+	}
+
+	DB.Find(&customers)
+
+	// 将查询结果映射到自定义结构
+	for _, customer := range customers {
+		customerData = append(customerData, struct {
+			ID uint `json:"id"`
+			Name string `json:"name"`
+			Bio  string `json:"bio"`
+		}{
+			ID: customer.ID,
+			Name: customer.Name,
+			Bio:  customer.Bio,
+		})
+	}
+
+	context.JSON(http.StatusOK, gin.H{"data": customerData})
+}
+
+
+func createCustomer(context *gin.Context) {
+
+	type CreateCustomerInput struct {
+		Name  string `json:"name" binding:"required"`
+		Bio   string `json:"bio" binding:"required"`
+	}
+
+	var input CreateCustomerInput
+	if err := context.ShouldBindJSON(&input); err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	// Create customer
+	customer := models.Customer{Name: input.Name,Bio: input.Bio}
+
+	DB.Create(&customer)
+
+	context.JSON(http.StatusOK, gin.H{"data": customer})
+}
+
+func getCustomer(context *gin.Context) {
+	customerID := context.Param("id")
+
+	var customer models.Customer
+
+	result := DB.First(&customer, customerID)
+	if result.Error != nil {
+		context.JSON(http.StatusNotFound, gin.H{"error": "Customer not found"})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{"data": customer})
+}
+
+func updateCustomer(context *gin.Context) {
+	customerID := context.Param("id")
+
+	var customer models.Customer
+
+	result := DB.First(&customer, customerID)
+	
+	if result.Error != nil {
+		context.JSON(http.StatusNotFound, gin.H{"error": "Customer not found"})
+		return
+	}
+
+
+	type UpdateCustomerInput struct {
+		Name  string `json:"name" binding:"required"`
+		Bio   string `json:"bio" binding:"required"`
+	}
+
+	var input UpdateCustomerInput
+	if err := context.ShouldBindJSON(&input); err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	DB.Model(&customer).Updates(input)
+	context.JSON(http.StatusOK, gin.H{"data": customer})
+}
+
+func deleteCustomer(context *gin.Context) {
+	customerID := context.Param("id")
+
+	var customer models.Customer
+
+	result := DB.First(&customer, customerID)
+	
+	if result.Error != nil {
+		context.JSON(http.StatusNotFound, gin.H{"error": "Customer not found"})
+		return
+	}
+
+	DB.Delete(&customer)
+
+	context.JSON(http.StatusOK, gin.H{"message": "deleted success"})
+}
+
+// ref: https://blog.logrocket.com/rest-api-golang-gin-gorm/
